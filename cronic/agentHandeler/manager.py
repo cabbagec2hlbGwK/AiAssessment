53def838bba0637890eb92d42ffd7339249f57f6
asher james <cabbageshell@protonmail.com>
2024-10-06 15:33:35 -0400

    req

--- cronic/agentHandeler/manager.py
+++ cronic/agentHandeler/manager.py
@@ -12,7 +12,7 @@ def main():
     secret_name = os.getenv("secret_name")
     rds_endpoint = os.getenv("rds_endpoint")
     region_name = os.getenv("AWS_DEFAULT_REGION")
-    agentManager = AgentManager(secret_name, rds_endpoint, region_name, db_name="aiAssesDB1")
+    agentManager = AgentManager(secret_name, rds_endpoint, region_name, db_name="aiAssesDB2")
     id = agentManager.crateUser(name="asher", email="asher@asher.com",endpoint="https://google.com",detailedReport=False)
     print(id)
     id = agentManager.crateUser(name="james", email="asher@asher.com",endpoint="https://google.com",detailedReport=False)
