b6c69219f5e208ab99470dedeeb4bfc944eee140
asher james <cabbageshell@protonmail.com>
2024-10-22 20:46:33 -0400

    sql bug

--- cronic/agentHandeler/manager.py
+++ cronic/agentHandeler/manager.py
@@ -34,22 +34,10 @@ def main():
     agentManager = AgentManager(secret_name, rds_endpoint, region_name, db_name="aiAssesDB2")
     endpoint = EndpointHandeler(host=hostEndpoint)
 
-    valid_user_ids = [
-    "c9d9dd6d-f32f-4518-b90f-624ae281b95e",
-    "54a96b2f-21b0-4dbb-be0b-750cd2c16aaf",
-    "abc7d8bf-196b-4455-b880-f55c80553387"
-    ]
-    valid_agent_ids = [
-        "89239373-c5e1-4fe4-aaa4-da5814119c88",
-        "5da75265-bcbe-42b4-8629-46c88dcc6e6d",
-        "bfaa3a74-b6e5-4c8d-8276-742728a00923"
-    ]
-    res = agentManager.createUser(name="john",email="john@gmail.com",endpoint="google.com", detailedReport=True)
-    print(res)
-    input()
+    #res = agentManager.createUser(name="john",email="john@gmail.com",endpoint="google.com", detailedReport=True)
     
 
-    deployTask(agentManager=agentManager, endpoint=endpoint, userId=valid_user_ids[1], targer="http://localhost:8000")
+    deployTask(agentManager=agentManager, endpoint=endpoint, userId="d57a5441-0d7f-46a4-a02a-726d747f7bb2", targer="http://localhost:8000")
 
 
     
