f3ae7f7f6622a9d4c0060c9e2ae5b4f68c9682d9
asher james <cabbageshell@protonmail.com>
2024-10-23 20:58:53 -0400

    new user ingester

--- cronic/agentHandeler/manager.py
+++ cronic/agentHandeler/manager.py
@@ -62,7 +62,7 @@ def main():
     hostEndpoint = os.getenv("APIHOST")
     agentManager = AgentManager(secret_name, rds_endpoint, region_name, db_name="aiAssesDB3")
     endpoint = EndpointHandeler(host=hostEndpoint)
-    handelNewRequest(agentManager, endpoint)
+    taskRotator(agentManager, endpoint)
 
     #res = agentManager.createUser(name="john",email="john@gmail.com",endpoint="google.com", detailedReport=True)
     
