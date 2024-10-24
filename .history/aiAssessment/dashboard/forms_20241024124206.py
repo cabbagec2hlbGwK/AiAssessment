fcfe08d33c038e93e93d8b9a7408d50c14854d04
Rahul Prasad <nagumantrirahul@gmail.com>
2024-10-24 14:27:02 -0400

    feat(UI): changes to new UI with cleaner design and customising

--- .history/aiAssessment/dashboard/forms_20241024124206.py
+++ .history/aiAssessment/dashboard/forms_20241024124206.py
@@ -7,7 +7,7 @@ class ReportForm(forms.Form):
         max_length=150,  # Adjust the max length as needed
         widget=forms.TextInput(attrs={
             'placeholder': 'Enter Username', 
-            'class': 'block w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-green-500 focus:ring-green-500 focus:ring-opacity-50 focus:outline-none'
+            'class': 'block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-green-500 focus:ring-green-500 focus:ring-opacity-50 focus:outline-none'
         }),
         label='Username',
     )
@@ -16,7 +16,7 @@ class ReportForm(forms.Form):
         required=True,
         widget=forms.EmailInput(attrs={
             'placeholder': 'Enter Email', 
-            'class': 'block w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-green-500 focus:ring-green-500 focus:ring-opacity-50 focus:outline-none'
+            'class': 'block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-green-500 focus:ring-green-500 focus:ring-opacity-50 focus:outline-none'
         }),
         label='Email',
     )
@@ -25,7 +25,7 @@ class ReportForm(forms.Form):
         required=True,
         widget=forms.URLInput(attrs={
             'placeholder': 'Enter Website URL', 
-            'class': 'block w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-green-500 focus:ring-green-500 focus:ring-opacity-50 focus:outline-none'
+            'class': 'block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:border-green-500 focus:ring-green-500 focus:ring-opacity-50 focus:outline-none'
         }),
         label='Website URL',
     )
@@ -39,10 +39,11 @@ class ReportForm(forms.Form):
         required=True,
         label='Select Report Type',
         widget=forms.Select(attrs={
-            'class': 'block w-full px-4 py-2 bg-white border border-gray-300 rounded-md focus:border-green-500 focus:ring-green-500 focus:ring-opacity-50 focus:outline-none'
+            'class': 'block w-full px-4 py-2 mt-2 bg-white border border-gray-300 rounded-md focus:border-green-500 focus:ring-green-500 focus:ring-opacity-50 focus:outline-none'
         }),
     )
 
     def clean_report_type(self):
         report_type = self.cleaned_data['report_type']
-        return int(report_type)
+        # Convert the report type to an integer
+        return int(report_type)  # Return 0 or 1
